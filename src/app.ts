f952d298892bfad6505db2b07bc70e02af4bf049
Usama Tayyab <ext.usama.tayyab@netigate.se>
2024-09-02 18:02:14 +0500

    404 error handled

--- src/app.ts
+++ src/app.ts
@@ -3,11 +3,15 @@ import { json } from 'body-parser';
 import userRoutes from './routes/userRoutes';
 import authRoutes from './routes/authRoutes';
 import errorMiddleware from './middleware/error/errorMiddleware';
+import { AppError } from './middleware/error/appError';
 
 const app = express();
 
 app.use(json());
 app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
+app.use((req, res, next) => {
+    next(new AppError('Not Found', 404));
+  });
 app.use(errorMiddleware);
 export default app;
